8ff64f8720e61adb2c54937f25ea46350b86040b
Atanas <uluchev@abv.bg>
2022-10-30 01:26:20 +0300

    add task

---
+++ Csharp-Advance/01.StacksAndQueues/01.ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                stack.Push(text[i]);
+            }
+            Console.WriteLine(string.Join("", stack));
+        }
+    }
+}
