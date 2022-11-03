d73a1e49149ccee44a3e993c817b70f95d11eea8
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-03 15:02:25 +0200

    Add files via upload

---
+++ OOP-CSharp/Encapsulation - Exercise/03. PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Elf elf = new Elf("nasko155", 45);
+            Console.WriteLine(elf);
+        }
+    }
+}
