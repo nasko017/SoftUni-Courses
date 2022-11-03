6e8ca2c1ac072dbd978b389564a876693c1074d9
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-03 02:36:55 +0200

    Add files via upload

---
+++ OOP-CSharp/Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMesseges.CannotBeZeroOrNegetive, nameof(this.Length)));
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMesseges.CannotBeZeroOrNegetive, nameof(Width)));
+                }
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMesseges.CannotBeZeroOrNegetive, nameof(this.Height)));
+                }
+                height = value;
+            }
+        }
+
+        private double SurfaceArea()
+        {
+            return 2 * (Length * Width) + 2 * (Length * Height) + 2 * (Height * Width);
+        }
+
+        private double LateralSurfaceArea()
+        {
+            return 2 * (Length * Height) + 2 * (Width * Height);
+        }
+
+        private double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {this.SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {this.Volume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+
+}
---
+++ OOP-CSharp/Encapsulation - Exercise/01.ClassBoxData/ExceptionMesseges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    static class ExceptionMesseges
+    {
+        public const string CannotBeZeroOrNegetive = "{0} cannot be zero or negative.";
+    }
+}
