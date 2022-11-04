934da5b8776553614786ec6d9e2fca27be50106d
Atanas <uluchev@abv.bg>
2022-11-04 17:57:23 +0200

    added a tasks

---
+++ Advance-CSharp/StacksAndQueues - Lab/04.PrintEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> evenNumbers = new Queue<int>();
+
+            int[] integersArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < integersArr.Length; i++)
+            {
+                if (integersArr[i] % 2 == 0)
+                {
+                    int evenNumber = integersArr[i];
+                    evenNumbers.Enqueue(evenNumber);
+                }
+            }
+            Console.WriteLine(String.Join(", ", evenNumbers));
+        }
+    }
+}
---
+++ Advance-CSharp/StacksAndQueues - Lab/07.TraficJam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TraficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
