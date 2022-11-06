6cc5760f32372c101b0c8c050d2a0e141c6ceae1
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-06 03:20:09 +0200

    Add files via upload

---
+++ OOP-CSharp/InterfacesAndAbstraction - Lab/02. Cars/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model,string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Model { get; private set; }   
+
+        public string Color { get; private set; } 
+
+        public string Start()
+        {
+            return " Start Engine";
+        }
+
+        public string Stop()
+        {
+            return "Stop Engine";
+        }
+        public override string ToString()
+        {
+            return $"{Color} {nameof(Seat)} {Model}";
+        }
+    }
+}
---
+++ OOP-CSharp/InterfacesAndAbstraction - Lab/02. Cars/Tesla.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar
+    {
+        public int Battery { get; private set; }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public Tesla(string model,string color,int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public string Start()
+        {
+            return "Start Engine";
+        }
+
+        public string Stop()
+        {
+            return "Stop Engine";
+        }
+        public override string ToString()
+        {
+            return $"{Color} {nameof(Tesla)} {Model} with {Battery}";
+        }
+    }
+}
