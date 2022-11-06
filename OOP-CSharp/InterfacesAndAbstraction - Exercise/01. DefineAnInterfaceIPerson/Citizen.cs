09c3a4977101cc02fb3e20f12350258b356c646c
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-06 21:06:24 +0200

    Add files via upload

---
+++ OOP-CSharp/InterfacesAndAbstraction - Exercise/01. DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson,IBirthable,IIdentifiable
+    {
+        public Citizen(string name,int age,string birthdate,string id)
+        {
+            Name = name;
+            Age = age;
+            Birthdate = birthdate;
+            Id = id;
+        }
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+        public string Id { get; private set; }
+    }
+}
