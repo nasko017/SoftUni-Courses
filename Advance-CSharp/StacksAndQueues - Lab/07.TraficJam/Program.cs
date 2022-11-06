dd2fcd36561e124efa9c95fc23943e6f5a253060
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-06 01:27:44 +0000

    Update Program.cs

--- Advance-CSharp/StacksAndQueues - Lab/07.TraficJam/Program.cs
+++ Advance-CSharp/StacksAndQueues - Lab/07.TraficJam/Program.cs
@@ -6,7 +6,36 @@ namespace TraficJam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> cars = new Queue<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            int passedCount= 0;
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            string car = cars.Dequeue();
+                            Console.WriteLine($"{car} passed!");
+                            passedCount++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+
+                }
+
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{passedCount} cars passed the crossroads.");
         }
     }
 }
