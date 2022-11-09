c76819692b349600a1f3ae9c14adc64faf83ac6d
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-09 19:29:03 +0200

    Add files via upload

---
+++ OOP-CSharp/Polymorphism - Lab/02. Animals/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ExplainSelf());
+            sb.Append("DJAAF");
+            return sb.ToString();
+        }
+    }
+}
