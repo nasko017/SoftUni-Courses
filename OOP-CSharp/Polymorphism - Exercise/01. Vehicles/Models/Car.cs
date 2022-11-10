907274b38ed2536073e889beb96533bb171534e2
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-10 20:09:43 +0200

    Add files via upload

---
+++ OOP-CSharp/Polymorphism - Exercise/01. Vehicles/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Vehicles.Models
+{
+    using Contracts;
+    public class Car : Vehicle
+    {
+        private const double FUEL_INCREMENT = 0.9;
+        public Car(double fuelQuantity, double fuelConsumption) : base(fuelQuantity, fuelConsumption, FUEL_INCREMENT)
+        {
+        }
+    }
+}
