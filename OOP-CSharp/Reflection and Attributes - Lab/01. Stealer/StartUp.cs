296cc9fb79281f01b0e89618fa04168e3148db69
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-23 01:35:39 +0200

    Update StartUp.cs

--- OOP-CSharp/Reflection and Attributes - Lab/01. Stealer/StartUp.cs
+++ OOP-CSharp/Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -7,12 +7,7 @@ namespace Stealer
     {
         static void Main(string[] args)
         {
-            //Type sbType = typeof(StringBuilder);
-            //
-            //StringBuilder sb = (StringBuilder)Activator.CreateInstance(sbType);
-            //
-            //StringBuilder sbb=(StringBuilder)Activator.CreateInstance(sbType, new object[] { 10});
-
+            
             Spy spy = new Spy();
             string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
             Console.WriteLine(result);
