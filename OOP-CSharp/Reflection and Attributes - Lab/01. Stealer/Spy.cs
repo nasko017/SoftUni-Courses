af988311bfb7a43e6ecdaf143f6737cfeb86738b
Atanas Uluchev <102230330+nasko017@users.noreply.github.com>
2022-11-23 01:34:33 +0200

    Add files via upload

---
+++ OOP-CSharp/Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] nameOfTheFields)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+            StringBuilder sb = new StringBuilder();
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {className}");
+
+            foreach (FieldInfo field in fields.Where(f => nameOfTheFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+            return sb.ToString().TrimEnd();
+
+        }
+        
+        
+    }
+}
---
+++ OOP-CSharp/Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Type sbType = typeof(StringBuilder);
+            //
+            //StringBuilder sb = (StringBuilder)Activator.CreateInstance(sbType);
+            //
+            //StringBuilder sbb=(StringBuilder)Activator.CreateInstance(sbType, new object[] { 10});
+
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+
+        }
+    }
+}
